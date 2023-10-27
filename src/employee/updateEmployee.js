d66bce698ca5f124edcaebd86551eef320d06d6f
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-10-27 11:03:30 +0530

    add

--- src/employee/updateEmployee.js
+++ src/employee/updateEmployee.js
@@ -1,41 +1,28 @@
 "use strict";
-
+import Joi from "joi";
 import { GetCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 export async function updateEmployee(event) {
+  // Create a DynamoDB client.
+  const client = new DynamoDBClient({});
+  // Create a DynamoDB Document Client from the base client.
+  const dynamo = DynamoDBDocumentClient.from(client);
 
+  try {
+    // Define a schema for validating the request body using Joi.
+    const employeeSchema = Joi.object({
+      employeeName: Joi.string().required(),
+      employeePhoneNumber: Joi.string().required(),
+    });
 
-    // Create a DynamoDB client.
-    const client = new DynamoDBClient({});
-    // Create a DynamoDB Document Client from the base client.
-    const dynamo = DynamoDBDocumentClient.from(client);
-  
-    try {
-      // Define a schema for validating the request body using Joi.
-      const employeeSchema = Joi.object({
-          employeeName: Joi.string().required(),
-          employeePhoneNumber: Joi.string().required(),
-      });
+    // Extract the employee ID from the path parameters.
+    let employeeID = event.pathParameters.employeeid;
 
-      // Extract the employee ID from the path parameters.
-      let employeeID = event.pathParameters.employeeid;
-
-      // Parse the request body as JSON.
-      const body = JSON.parse(event.body);
-      console.log(body);
-
-      // Check if the request body is null.
-      if (body == null) {
-          // Return a 400 status code with an error message.
-          return {
-              statusCode: 400,
-              body: JSON.stringify({
-                  message: " error",
-              }),
-          };
-      }
+    // Parse the request body as JSON.
+    const body = JSON.parse(event.body);
+    console.log(body);
 
     let value;
     try {
@@ -56,7 +43,7 @@ export async function updateEmployee(event) {
           SK: employeeID,
         },
       };
-       // Retrieve the existing employee record from DynamoDB.
+      // Retrieve the existing employee record from DynamoDB.
       const existingemployee = await dynamo.send(new GetCommand(params));
 
       console.log("existingemployee", existingemployee);
@@ -68,7 +55,7 @@ export async function updateEmployee(event) {
           }),
         };
       } else {
-         // Define the data to be updated in DynamoDB.
+        // Define the data to be updated in DynamoDB.
         const insertData = {
           TableName: process.env.TABLE_EMPLOYEE,
           Key: {
@@ -87,7 +74,7 @@ export async function updateEmployee(event) {
           },
           ReturnValues: "ALL_NEW",
         };
-       // Update the employee data in DynamoDB using an UpdateCommand.
+        // Update the employee data in DynamoDB using an UpdateCommand.
         return dynamo.send(new UpdateCommand(insertData)).then((data) => {
           console.log(data);
           return {
@@ -100,7 +87,7 @@ export async function updateEmployee(event) {
       }
     }
   } catch (error) {
-     // If any other error occurs, return a 400 status code with an error message.
+    // If any other error occurs, return a 400 status code with an error message.
     return {
       statusCode: 400,
       body: JSON.stringify({
